2d8622790f0a93fd7d8c204187e15f0de20bb77f
Martmists <martmists@gmail.com>
2019-12-14 01:44:04 +0100

    1.15

--- src/main/java/com/martmists/nowatercaves/mixin/WaterCaveMixin.java
+++ src/main/java/com/martmists/nowatercaves/mixin/WaterCaveMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.carver.Carver;
 import net.minecraft.world.gen.carver.UnderwaterCaveCarver;
-import net.minecraft.world.gen.carver.UnderwaterRavineCarver;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,8 +14,8 @@ import java.util.Random;
 
 @Mixin(UnderwaterCaveCarver.class)
 public class WaterCaveMixin {
-    @Inject(method = "carveAtPoint", at = @At("HEAD"), cancellable = true)
-    static void carveAtPoint(Carver<?> carver_1, Chunk chunk_1, BitSet bitSet_1, Random random_1, BlockPos.Mutable blockPos$Mutable_1, int int_1, int int_2, int int_3, int int_4, int int_5, int int_6, int int_7, int int_8, CallbackInfoReturnable<Boolean> info){
+    @Inject(method = "carveAtPoint(Lnet/minecraft/world/gen/carver/Carver;Lnet/minecraft/world/chunk/Chunk;Ljava/util/BitSet;Ljava/util/Random;Lnet/minecraft/util/math/BlockPos$Mutable;IIIIIIII)Z", at = @At("HEAD"), cancellable = true)
+    private static void carveAtPoint(Carver<?> carver, Chunk chunk, BitSet mask, Random random, BlockPos.Mutable pos, int seaLevel, int mainChunkX, int mainChunkZ, int x, int z, int relativeX, int y, int relativeZ, CallbackInfoReturnable<Boolean> info){
         info.setReturnValue(false);
     }
 }
